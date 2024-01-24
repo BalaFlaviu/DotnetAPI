f0d2d558a259b26958e3a478d21148ff4ad52ee2
Flaviu Bala <flaviu@inspirocode.ro>
2024-01-24 14:29:00 +0200

    Add project files.

---
+++ Models/UserJobInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace DotnetAPI.Models
+{
+    public partial class UserJobInfo
+    {
+        public int UserId { get; set; }
+        public string JobInfo { get; set; }
+        public string Department { get; set; }
+
+        public UserJobInfo()
+        {
+            if (JobInfo == null)
+            {
+                JobInfo = "";
+            }
+
+            if (Department == null)
+            {
+                Department = "";
+            }
+        }
+
+    }
+}
---
+++ Models/UserSalary.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetAPI.Models
+{
+    public partial class UserSalary
+    {
+        public int UserId { get; set; }
+        public decimal salary { get; set; }
+
+    }
+}
